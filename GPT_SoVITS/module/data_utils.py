53a849eb65682ed82b05ffe5a6f853e052d62209
Jungyong Han <hjyjj554@koroad.or.kr>
2025-05-05 12:09:59 +0900

    refactor(data_utils): add try-catch block for debugging print statement
    The try-catch block was added to handle potential errors when printing the debug statement, ensuring the function continues to execute even if the print operation fails. This improves the robustness of the code during debugging.

--- GPT_SoVITS/module/data_utils.py
+++ GPT_SoVITS/module/data_utils.py
@@ -135,7 +135,10 @@ class TextAudioSpeakerLoader(torch.utils.data.Dataset):
 
     def __getitem__(self, index):
         # with torch.no_grad():
-
+        try:
+            print(f"data_loader:{index}/{len(self.audiopaths_sid_text)}")
+        except:
+            pass
         return self.get_audio_text_speaker_pair(self.audiopaths_sid_text[index])
 
     def __len__(self):
